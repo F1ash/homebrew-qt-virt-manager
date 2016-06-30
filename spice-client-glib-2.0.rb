d8492d6bbf005a80380c466fd81727200f3b132c
F1ash <kaperang07@gmail.com>
2016-06-30 10:20:37 +0300

    fixed spice-client-glib.rb description

--- spice-client-glib-2.0.rb
+++ spice-client-glib-2.0.rb
@@ -1,5 +1,5 @@
 class SpiceClientGlib2 < Formula
-  desc "Glib client/common libraries for SPICE"
+  desc "Glib2 client/common libraries for SPICE"
   homepage "http://www.spice-space.org"
   url "http://www.spice-space.org/download/gtk/spice-gtk-0.31.tar.bz2"
   sha256 "c72b4d202b1c0b71d6e24ce5caf914d6dddbcf4010d10db9c2d8e73af728c1ca"
