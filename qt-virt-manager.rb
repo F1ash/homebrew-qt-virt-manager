f5e2361b3466c5057d75532b06e9bfeaaaa3d917
F1ash <kaperang07@gmail.com>
2016-09-13 20:54:55 +0300

    update git point for build;

--- qt-virt-manager.rb
+++ qt-virt-manager.rb
@@ -2,9 +2,9 @@ class QtVirtManager < Formula
   desc "Qt5 Application for managing virtual machines"
   homepage "http://f1ash.github.io/qt-virt-manager/"
   url "https://github.com/F1ash/qt-virt-manager.git",
-  :revision => "bf85c487b816d10846af9c7967541f781882624d",
+  :revision => "0506cd91943c1bb4d32487c7f10fe913be507393",
   :using => :git
-  version "0.34.50.dev20.07" # random version
+  version "0.34.50.dev07.09" # random version
   #sha256 "???"
 
   depends_on "intltool" => :build
