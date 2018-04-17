2b40a73a7cf178dfbdc478d115a15afbdbea1625
F1ash <kaperang07@gmail.com>
2018-04-17 23:38:03 +0300

    fixed Libxml2Pythonless class in Formula;

--- libxml2pythonless.rb
+++ libxml2pythonless.rb
@@ -1,4 +1,4 @@
-class Libxml2Pythonless < Formula
+class Libxml2pythonless < Formula
   desc "GNOME XML library"
   homepage "http://xmlsoft.org/"
   url "http://xmlsoft.org/sources/libxml2-2.9.7.tar.gz"
