8bc9e8517891dabbff0053f806a4491f404f184d
F1ash <kaperang07@gmail.com>
2019-07-22 12:45:03 +0300

    spice version updated;

--- spice-client-glib2.rb
+++ spice-client-glib2.rb
@@ -1,8 +1,8 @@
 class SpiceClientGlib2 < Formula
   desc "Glib2 client/common libraries for SPICE"
   homepage "http://www.spice-space.org"
-  url "https://www.spice-space.org/download/gtk/spice-gtk-0.35.tar.bz2"
-  sha256 "b4e6073de5125e2bdecdf1fbe7c9e8c4cabe9c85518889b42f72bf63c8ab9e86"
+  url "https://www.spice-space.org/download/gtk/spice-gtk-0.37.tar.bz2"
+  sha256 "1f28b706472ad391cda79a93fd7b4c7a03e84b88fc46ddb35dddbe323c923bb7"
 
   depends_on "intltool" => :build
   depends_on "libtool" => :build
--- spice-protocol.rb
+++ spice-protocol.rb
@@ -1,8 +1,8 @@
 class SpiceProtocol < Formula
   desc "Headers for SPICE protocol"
   homepage "https://www.spice-space.org/"
-  url "https://www.spice-space.org/download/releases/spice-protocol-0.12.14.tar.bz2"
-  sha256 "20350bc4309039fdf0d29ee4fd0033cde27bccf33501e13b3c1befafde9d0c9c"
+  url "https://www.spice-space.org/download/releases/spice-protocol-0.14.0.tar.bz2"
+  sha256 "b6a4aa1ca32668790b45a494bbd000e9d05797b391d5a5d4b91adf1118216eac"
 
   def install
     system "./configure", "--disable-silent-rules",
